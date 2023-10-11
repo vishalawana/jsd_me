17f2839fc7e41042e91dff418c35c3e033752900
vishal awana <53907738+vishalawana@users.noreply.github.com>
2023-10-11 17:29:46 +0000

    fact done iteration

---
+++ algosinjs/02_mathalgos.js
@@ -0,0 +1,30 @@
+//fibbonacci series
+// function fib(n){
+//     const fibo = [0,1];
+//     for(let i = 2;i<n;i++)
+//     {
+//         fibo[i] = fibo[i-1]+fibo[i-2];
+//     }
+//     return fibo;
+// }
+// console.log(fib(2))
+// console.log(fib(6))
+// console.log(fib(1))
+// console.log(fib(0))
+// console.log(fib(3))
+// <----------------------------->
+// factorial of a Number
+// function factorial(n)
+// {
+//     let fact = 1;
+//     for(let i = 1;i<=n;i++)
+//     {
+//         fact = fact*i;
+//     }
+//     return fact;
+
+// }
+// console.log(factorial(1))
+// console.log(factorial(0))
+// console.log(factorial(4))
+// console.log(factorial(6))
