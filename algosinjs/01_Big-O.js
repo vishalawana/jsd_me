871fe538f9557a827ea8ccc0ed95eb0f0633ef21
vishal awana <53907738+vishalawana@users.noreply.github.com>
2023-10-11 16:08:55 +0000

    done

---
+++ algosinjs/01_Big-O.js
@@ -0,0 +1,23 @@
+// Object big-O notations
+
+
+// insert- O(1)
+// Remove- O(1)
+// Access- O(1)
+// search- O(n)
+// Object.keys()- O(n)
+// Object.values()- O(n)
+// Object.entries()- O(n)
+
+
+// <-------------------------->
+// Array big-O notations
+
+
+// insert/remove at end - O(1)
+// insert/remove at beginning - O(n)
+// access- O(1)
+// search- O(n)
+// push/pop- O(1)
+// shift/unshift/concat/slice/splice- O(n)
+// forEach/map/filter/reduce- O(n)
